fc4320dc2724c506eaf9b578a1c18f7f85004a06
Zanckor <zanckor002@gmail.com>
2024-05-08 11:52:28 +0200

    Changed color of SWAMP POTION to blue

--- src/main/java/cybercat5555/faunus/mixin/PotionColorMixin.java
+++ src/main/java/cybercat5555/faunus/mixin/PotionColorMixin.java
@@ -1,20 +1,33 @@
 package cybercat5555.faunus.mixin;
 
 import cybercat5555.faunus.core.PotionRegistry;
-import net.minecraft.potion.Potion;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.potion.PotionUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 @Mixin(PotionUtil.class)
 public class PotionColorMixin {
 
-    @Inject(method = "getColor(Lnet/minecraft/potion/Potion;)I", at = @At("HEAD"), cancellable = true)
-    private static void get(Potion potion, CallbackInfoReturnable<Integer> cir) {
-        if (potion.equals(PotionRegistry.SWAM_CLAWLER_POTION)) {
-            cir.setReturnValue(49257);
+    @Inject(method = "getColor(Ljava/util/Collection;)I", at = @At("HEAD"), cancellable = true)
+    private static void get(Collection<StatusEffectInstance> effects, CallbackInfoReturnable<Integer> cir) {
+
+        if (effects.containsAll(swampCrawlerEffects)) {
+            cir.setReturnValue(3114217);
         }
     }
+
+    private static final Collection<StatusEffectInstance> swampCrawlerEffects = Arrays.asList(
+        new StatusEffectInstance(StatusEffects.RESISTANCE, 1200),
+        new StatusEffectInstance(StatusEffects.WATER_BREATHING, 1200),
+        new StatusEffectInstance(StatusEffects.LUCK, 1200),
+        new StatusEffectInstance(StatusEffects.HUNGER, 1200),
+        new StatusEffectInstance(StatusEffects.WEAKNESS, 1200)
+    );
 }
