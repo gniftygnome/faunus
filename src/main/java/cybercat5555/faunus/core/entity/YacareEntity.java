4ab6712f6d3e3d94dd9558c7cac099ecffa50d05
Zanckor <zanckor002@gmail.com>
2024-05-09 13:24:10 +0200

    Added attributes to yacare, goals, movecontrol, breed items, and animations

--- src/main/java/cybercat5555/faunus/core/entity/YacareEntity.java
+++ src/main/java/cybercat5555/faunus/core/entity/YacareEntity.java
@@ -3,8 +3,12 @@ package cybercat5555.faunus.core.entity;
 import cybercat5555.faunus.core.EntityRegistry;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.goal.ActiveTargetGoal;
-import net.minecraft.entity.ai.goal.RevengeGoal;
+import net.minecraft.entity.ai.control.AquaticMoveControl;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.WaterCreatureEntity;
 import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -19,12 +23,20 @@ import software.bernie.geckolib.core.object.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class YacareEntity extends AnimalEntity implements GeoEntity {
-    protected static final RawAnimation IDLE_ANIM = RawAnimation.begin().thenLoop("idle");
+    protected static final RawAnimation IDLE_LAND_ANIM = RawAnimation.begin().thenLoop("idle_land");
+    protected static final RawAnimation IDLE_WATER_ANIM = RawAnimation.begin().thenLoop("idle_water");
+    protected static final RawAnimation SWIM_ANIM = RawAnimation.begin().thenLoop("swim");
+    protected static final RawAnimation RUSH_WATER_ANIM = RawAnimation.begin().thenLoop("rush_water");
+    protected static final RawAnimation WALK_ANIM = RawAnimation.begin().thenLoop("walk");
+    protected static final RawAnimation RUSH_LAND_ANIM = RawAnimation.begin().thenLoop("rush_land");
+    protected static final RawAnimation DEATH_ANIM = RawAnimation.begin().thenLoop("WIP death roll3");
+
 
     private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
 
     public YacareEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world);
+        this.moveControl = new AquaticMoveControl(this, 85, 10, 2F, 1F, true);
     }
 
     @Override
@@ -32,11 +44,30 @@ public class YacareEntity extends AnimalEntity implements GeoEntity {
         return EntityRegistry.YACARE.create(world);
     }
 
+    public static DefaultAttributeContainer.Builder createMobAttributes() {
+        return MobEntity.createMobAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 24f)
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.35f)
+                .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 6f)
+                .add(EntityAttributes.GENERIC_ATTACK_KNOCKBACK, 0.3f)
+                .add(EntityAttributes.GENERIC_ATTACK_SPEED, 1f);
+    }
+
     @Override
     protected void initGoals() {
+        goalSelector.add(1, new MeleeAttackGoal(this,  isSubmergedInWater() ? 3 : 1.35f, false));
+        goalSelector.add(2, new LookAroundGoal(this));
+        goalSelector.add(3, new WanderAroundGoal(this, 0.7D));
         targetSelector.add(1, new RevengeGoal(this));
         targetSelector.add(2, new ActiveTargetGoal<>(this, LivingEntity.class, true,
-                target -> target.getBoundingBox().getAverageSideLength() < getBoundingBox().getAverageSideLength()));
+                target -> target instanceof WaterCreatureEntity && target.getBoundingBox().getAverageSideLength() < getBoundingBox().getAverageSideLength()));
+    }
+
+    @Override
+    public void tick() {
+        setAir(getMaxAir());
+
+        super.tick();
     }
 
     @Override
@@ -50,6 +81,15 @@ public class YacareEntity extends AnimalEntity implements GeoEntity {
     }
 
     protected <E extends YacareEntity> PlayState idleAnimController(final AnimationState<E> event) {
+        boolean isAttacking = isAttacking();
+        boolean isMoving = event.isMoving();
+
+        if (isSubmergedInWater()) {
+            event.setAndContinue(isMoving && isAttacking ? RUSH_WATER_ANIM : isMoving ? SWIM_ANIM : IDLE_WATER_ANIM);
+        } else {
+            event.setAndContinue(isMoving && isAttacking ? RUSH_LAND_ANIM : isMoving ? WALK_ANIM : IDLE_LAND_ANIM);
+        }
+
         return PlayState.CONTINUE;
     }
 }
