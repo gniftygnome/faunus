61a3328c7e79cae520555f97d7dcd4f1b506117d
Zanckor <zanckor002@gmail.com>
2024-06-25 14:43:34 +0200

    Added potion recipes and item recipes

--- src/main/java/cybercat5555/faunus/core/PotionRegistry.java
+++ src/main/java/cybercat5555/faunus/core/PotionRegistry.java
@@ -3,6 +3,8 @@ package cybercat5555.faunus.core;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+import net.minecraft.recipe.BrewingRecipeRegistry;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -19,18 +21,32 @@ public class PotionRegistry {
             new StatusEffectInstance(StatusEffects.HUNGER, 1200),
             new StatusEffectInstance(StatusEffects.WEAKNESS, 1200)));
 
-    public static final Potion THROWABLE_LEECHING = registerPotion("throwable_leeching", new Potion(
-            new StatusEffectInstance(EffectStatusRegistry.STOP_HEALING_EFFECT, 1200)));
+    public static final Potion RANCID = registerPotion("rancid", new Potion(
+            new StatusEffectInstance(EffectStatusRegistry.RANCID_EFFECT, 1200)));
 
-    public static final Potion DRINKABLE_LEECHING = registerPotion("drinkable_leeching", new Potion(
-            new StatusEffectInstance(EffectStatusRegistry.CLEAR_EFFECTS_EFFECT, 10)));
+    public static final Potion STINKY = registerPotion("stinky", new Potion(
+            new StatusEffectInstance(EffectStatusRegistry.STINKY_EFFECT, 1200)));
+
+    public static final Potion LEECHING = registerPotion("leeching", new Potion(
+            new StatusEffectInstance(EffectStatusRegistry.STOP_HEALING_EFFECT, 40, 1, false, false, false)));
 
 
     public static Potion registerPotion(String name, Potion potion) {
         return Registry.register(Registries.POTION, new Identifier(MODID, name), potion);
     }
 
+    public static void registerRecipes() {
+        LOG.info("Registering potion recipes for " + MODID);
+
+        BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, ItemRegistry.HOATZIN_FEATHER, RANCID);
+        BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, ItemRegistry.BOTTLED_STINK, STINKY);
+        BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, ItemRegistry.BLUE_CRAYFISH, SWAMP_CRAWLER);
+        BrewingRecipeRegistry.registerPotionRecipe(Potions.AWKWARD, ItemRegistry.BOTTLED_LEECH, LEECHING);
+    }
+
     public static void init() {
         LOG.info("Registering potions for " + MODID);
+
+        registerRecipes();
     }
 }
---
+++ src/main/java/cybercat5555/faunus/mixin/DrinkablePotionMixin.java
@@ -0,0 +1,52 @@
+package cybercat5555.faunus.mixin;
+
+import cybercat5555.faunus.core.EffectStatusRegistry;
+import cybercat5555.faunus.core.ItemRegistry;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PotionItem;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Iterator;
+
+@Mixin(PotionItem.class)
+public class DrinkablePotionMixin {
+
+    @Inject(method = "finishUsing", at = @At("HEAD"), cancellable = true)
+    private void finishUsingMixin(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
+        // If item is leeching potion, and is a drinkable potion apply clear effects
+        if (stack.getItem() == ItemRegistry.BOTTLED_LEECH) {
+            clearNegativeEffects(user);
+            stack.decrement(1);
+
+            cir.setReturnValue(stack);
+        }
+
+        user.addStatusEffect(new StatusEffectInstance(EffectStatusRegistry.LEECHING_EFFECT, 1200));
+    }
+
+    @Unique
+    private void clearNegativeEffects(LivingEntity user) {
+        if (user.getWorld().isClient) {
+            return;
+        }
+
+        Iterator<StatusEffectInstance> iterator = user.getActiveStatusEffects().values().iterator();
+
+        while (iterator.hasNext()) {
+            StatusEffectInstance effect = iterator.next();
+
+            if (!effect.getEffectType().isBeneficial()) {
+                user.removeStatusEffect(effect.getEffectType());
+            }
+
+            iterator.remove();
+        }
+    }
+}
