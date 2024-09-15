efc4862e6b68fd3e4903862df9190c997ca54aaa
Zanckor <zanckor002@gmail.com>
2024-09-15 14:34:05 +0200

    Optimized HangTreeGoal. Though it was the problem, but seems it is not

--- src/main/java/cybercat5555/faunus/core/entity/ai/goals/HangTreeGoal.java
+++ src/main/java/cybercat5555/faunus/core/entity/ai/goals/HangTreeGoal.java
@@ -1,6 +1,8 @@
 package cybercat5555.faunus.core.entity.ai.goals;
 
 import cybercat5555.faunus.core.entity.livingEntity.CapuchinEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LeavesBlock;
 import net.minecraft.entity.ai.NoPenaltyTargeting;
 import net.minecraft.entity.ai.goal.WanderAroundGoal;
 import net.minecraft.entity.mob.PathAwareEntity;
@@ -9,7 +11,10 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
+import java.util.HashMap;
+
 public class HangTreeGoal extends WanderAroundGoal {
+    private static final HashMap<BlockPos, BlockState> BLOCK_STATE_CACHE = new HashMap<>();
     private boolean isHanging;
     private BlockPos hangingSpot;
 
@@ -44,14 +49,29 @@ public class HangTreeGoal extends WanderAroundGoal {
     @Override
     public void tick() {
         if (!isHanging) {
-            findStarterHangingSpot();
-
-            if (hangingSpot != null) {
+            if (hangingSpot == null && this.mob.getWorld().getTime() % 5 == 0) {
+                findStarterHangingSpot();
+            } else {
                 moveToHangingSpot();
+            }
+
+        } else {
+            boolean shouldRestart = shouldRestartHangingSpot();
+
+            if (shouldRestart) {
+                hangingSpot = null;
+                isHanging = false;
             }
         }
 
         hangingTreeHandler();
+    }
+
+    private boolean shouldRestartHangingSpot() {
+        if (hangingSpot == null) return false;
+
+        double distanceToHangingSpot = this.mob.getBlockPos().getSquaredDistance(hangingSpot);
+        return distanceToHangingSpot > 32;
     }
 
     private void hangingTreeHandler() {
@@ -108,7 +128,7 @@ public class HangTreeGoal extends WanderAroundGoal {
 
 
     private void moveToHangingSpot() {
-        if (this.mob.getWorld().getBlockState(hangingSpot).isAir()) {
+        if (getCachedBlockState(hangingSpot).isAir()) {
             isHanging = false;
             hangingSpot = null;
             return;
@@ -125,9 +145,11 @@ public class HangTreeGoal extends WanderAroundGoal {
 
 
             // Move this.entity to treePos
-            this.mob.getNavigation().startMovingTo(hangingSpot.getX(), hangingSpot.getY(), hangingSpot.getZ(), 1.0);
-            double horizontalDistance = Math.sqrt(hangingSpot.getSquaredDistance(this.mob.getX(), hangingSpot.getY(), this.mob.getZ()));
+            if (!this.mob.getNavigation().isFollowingPath()) {
+                this.mob.getNavigation().startMovingTo(hangingSpot.getX(), hangingSpot.getY(), hangingSpot.getZ(), 1.0);
+            }
 
+            double horizontalDistance = Math.sqrt(hangingSpot.getSquaredDistance(this.mob.getX(), hangingSpot.getY(), this.mob.getZ()));
             if (horizontalDistance <= 1.5 && verticalDistance <= 3 &&
                     mob.isOnGround() && !isHanging) {
                 // Jump from ground to tree block
@@ -150,15 +172,14 @@ public class HangTreeGoal extends WanderAroundGoal {
 
     private void findStarterHangingSpot() {
         BlockPos pos = mob.getBlockPos();
-        BlockPos treePos = hangingSpot != null ? hangingSpot : findNearestTree(pos, 8);
+        BlockPos treePos = hangingSpot != null ? hangingSpot : findNearestTree(pos, 6);
 
         if (treePos != null) {
             Vec3d treeVec = treePos.toCenterPos();
-            boolean hasTwoBlockSpace = this.mob.getWorld().getBlockState(treePos.down(2)).isAir();
+            boolean hasTwoBlockSpace = getCachedBlockState(treePos.down(2)).isAir();
             boolean isHigherThanMob = treeVec.y > this.mob.getY();
 
             if (hasTwoBlockSpace && isHigherThanMob) {
-                this.mob.getNavigation().startMovingTo(treeVec.x, treeVec.y, treeVec.z, 1.0);
                 hangingSpot = treePos;
             }
         } else {
@@ -171,9 +192,13 @@ public class HangTreeGoal extends WanderAroundGoal {
             for (int yPos = -radius; yPos < radius; yPos++) {
                 for (int zPos = -radius; zPos < radius; zPos++) {
                     BlockPos blockPos = pos.add(xPos, yPos, zPos);
+                    BlockState leavesBlock = getCachedBlockState(blockPos);
+                    BlockState belowBlock = getCachedBlockState(blockPos.down());
 
-                    if (this.mob.getWorld().getBlockState(blockPos).getBlock().getTranslationKey().contains("leave")
-                            && this.mob.getWorld().getBlockState(blockPos.down()).isAir()) {
+                    boolean isLeaves = leavesBlock.getBlock() instanceof LeavesBlock;
+                    boolean isBelowAir = belowBlock.isAir();
+
+                    if (isLeaves && isBelowAir) {
                         return blockPos;
                     }
                 }
@@ -183,14 +208,32 @@ public class HangTreeGoal extends WanderAroundGoal {
         return null;
     }
 
+    private BlockState getCachedBlockState(BlockPos pos) {
+        if(pos == null) pos = this.mob.getBlockPos();
+
+        if (BLOCK_STATE_CACHE.containsKey(pos)) {
+            BlockState blockState = BLOCK_STATE_CACHE.get(pos);
+            if(this.mob.getWorld().getTime() % 2400 == 0) {
+                BLOCK_STATE_CACHE.remove(pos);
+            }
+
+            return blockState;
+        } else {
+            BlockState blockState = this.mob.getWorld().getBlockState(pos);
+            BLOCK_STATE_CACHE.put(pos, blockState);
+
+            return blockState;
+        }
+    }
+
     private boolean isHangingTree(BlockPos pos) {
-        return this.mob.getWorld().getBlockState(pos).getBlock().getTranslationKey().contains("leave");
+        return getCachedBlockState(pos).getBlock().getTranslationKey().contains("leave");
     }
 
     private boolean isHangingTree(Vec3d pos) {
         BlockPos blockPos = new BlockPos((int) pos.x, (int) pos.y, (int) pos.z);
 
-        return this.mob.getWorld().getBlockState(blockPos).getBlock().getTranslationKey().contains("leave");
+        return getCachedBlockState(blockPos).getBlock().getTranslationKey().contains("leave");
     }
 
     @Override
